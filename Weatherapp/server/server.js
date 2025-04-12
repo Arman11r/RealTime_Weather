0d2cb93f886e7f62fe4a968806b10bdc22082607
Arman Ahmed <137074653+Arman11r@users.noreply.github.com>
2025-04-12 17:06:51 +0530

    Update server.js

--- Weatherapp/server/server.js
+++ Weatherapp/server/server.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
 
-const weatherRoute = require('./routes/weather');
-app.use('/api/weather', weatherRoute);
-
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin: ["http://localhost:3000","https://real-time-weather-ndwd.vercel.app/"],
-  methods: ["GET", "POST"],
-};
 
-app.use(cors(corsOptions));
-app.use('/weather', weatherRoute);
+app.use(cors());
+app.use(express.json());
+
+
+const weatherRoute = require("./routes/weather");
+app.use("/api/weather", weatherRoute);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
